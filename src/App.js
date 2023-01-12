fde7d13d2fbabd63ae8e812f39cb2912a241ca94
unknown <iambmanohar@gmail.com>
2023-01-12 23:21:14 +0530

    manohar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
  import Hello from './components/Hello'
 import Venkat from './components/Venkat'
+import Manohar from './components/Manohar'
 function App(){
     return <div> 
         <h1>App, Compounent</h1>
         <hr />
+        <hr />
+        <h1>Manohar</h1>
+        <Manohar />
+        <hr />
+        <hr />
         <Hello />   
+        <h1>hai </h1>
 <br />
         <hr />   
+        <hr />   
+
         <Venkat /> 
+
         </div>
         
 }    
---
+++ src/components/Manohar.js
@@ -0,0 +1,17 @@
+function Manohar (){
+    let message = "i'm Manohar"
+    let image= "https://static-koimoi.akamaized.net/wp-content/new-galleries/2022/04/mahesh-babu-does-it-again-comes-to-aid-of-30-suffering-children-001.jpg"
+    return <div>
+        <h1>Hello , compounant</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit recusandae sequi rerum, nobis laborum reprehenderit?</p>
+<h3>message    :    {message}</h3>
+<img src={image} alt=""/> 
+
+
+    </div>
+}
+export default Manohar
+
+
+
+
