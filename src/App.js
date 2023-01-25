04f624a5ae8527d7167e2a2c7db5fc635a331b7b
unknown <iambmanohar@gmail.com>
2023-01-25 13:28:27 +0530

    data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 /*  import Hello from './components/Hello'
 import Venkat from './components/Venkat'
-import Manohar from './components/Manohar' */
-// import Manohar from './components/Manohar';
-// import Class1 from './components/class2class/Class1';
-import Product from './components/produts/product';
+import Manohar from './components/Manohar' 
+import Manohar from './components/Manohar';
+import Class1 from './components/class2class/Class1'; */
+import Class1 from './components/class2class/Class1'; 
+// import Product from './components/produts/product';
 
 function App(){
     return <div> 
-        {/* <h1> Class 1</h1> */}
+         <h1> Class 1</h1> 
 <hr />
        <h2> product component</h2>
 
-        <Product />
+        <Class1/>
  
         </div>
         
