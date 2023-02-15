b81c721f8623d53265b744352a2bec201dc4b969
unknown <iambmanohar@gmail.com>
2023-02-15 13:17:27 +0530

    data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router , Route , Routes } from 'react-router-dom'
-import Navbar from './practice/Navbar'
+import ETApp from './practice/ETApp'
  class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-<Navbar/>
-        </Router>
+<ETApp/>
       </div>
     )
   }
